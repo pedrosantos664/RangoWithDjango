4db2a48e8efb9169bfdcda1547a5cc9c7f2ec12f
pedrosantos664 <124084338+pedrosantos664@users.noreply.github.com>
2025-08-27 17:45:57 -0300

    Update settings.py

--- rango/settings.py
+++ rango/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'janela/templates/janela'),  # Agora inclui templates
+    os.path.join(BASE_DIR, 'janela/templates/janela'),  
 ]
 
 
